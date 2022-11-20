2ca5687a40abe33a09b5c3a0e4158eb86291b291
Danilo Pinheiro <nilouco@gmail.com>
2022-11-20 20:00:23 -0300

    N521 - Fixed Chain building issue
    N521 - Fixed Chain module building issue for rotated guides.
    Just changed when create the ikCtrl parentConstraint to clusterNode and using the ik last control to define the upLocator for the aimConstraint.

--- dpAutoRigSystem/dpAutoRig.py
+++ dpAutoRigSystem/dpAutoRig.py
@@ -19,8 +19,8 @@
 
 
 # current version:
-DPAR_VERSION_PY3 = "4.01.14"
-DPAR_UPDATELOG = "N366 - Import calibration progress window."
+DPAR_VERSION_PY3 = "4.01.15"
+DPAR_UPDATELOG = "N551 - Fixed Chain building issue."
 
 
 
