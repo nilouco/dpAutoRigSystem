67c3c7f4e1f7f01df179a261b56e374f17857ef3
Danilo Pinheiro <nilouco@gmail.com>
2023-02-16 17:59:54 -0300

    N598 CustomAttr wip spreadSheetEditor
    WIP changing the UI to use the spreadSheetEditor.
    Need to find a way to load only existing nodes with dp* attr.

--- dpAutoRigSystem/Extras/dpCustomAttr.py
+++ dpAutoRigSystem/Extras/dpCustomAttr.py
@@ -39,18 +39,47 @@ class CustomAttr(object):
         self.itemList = []
         # call main UI function
         if self.ui:
+            self.itemF = self.getItemFilter()
             self.closeUI()
             self.mainUI()
-            self.refreshUI()
-            self.jobChangeSelection()
-            
 
-    def refreshUI(self, *args):
-        """ Just call populate UI and actualize layout methodes.
+
+    def getItemFilter(self, *args):
         """
-        self.populateItems()
-#        self.populateAttrs()
-#        self.actualizeEditLayout()
+        """
+        itemF = cmds.itemFilter(byType="transform")
+        for ignoreIt in IGNORE_LIST:
+            itemF = cmds.itemFilter(difference=(itemF, cmds.itemFilter(byName=ignoreIt)))
+        return itemF
+
+
+    def changeItemSelectionConnection(self, *args):
+        """
+        """
+        # get current selection type (all, selected or existing custom attributes)
+        selectionRadioButton = cmds.radioCollection(self.selectionCollection, query=True, select=True)
+        radioButtonAnnotation = cmds.radioButton(selectionRadioButton, query=True, annotation=True)
+        # fill conform
+        if radioButtonAnnotation == "all":
+            itemSC = cmds.selectionConnection(worldList=True)
+            print("aaallll")
+        elif radioButtonAnnotation == "selected":
+            itemSC = cmds.selectionConnection(activeList=True)
+            print("sellll")
+#        elif radioButtonAnnotation == "existing":
+
+#            self.updateItemsList(self.getExistingList())
+
+#            self.itemSC = 
+        cmds.spreadSheetEditor(self.mainSSE, forceMainConnection=itemSC, edit=True)
+        
+
+    def getAttrFilter(self, *args):
+        """
+        """
+        attrIFA = cmds.itemFilterAttr(byName=ATTR_START+"*")
+
+
 
         
     def closeUI(self, *args):
@@ -68,56 +97,33 @@ class CustomAttr(object):
         customAttributes_winWidth  = 380
         customAttributes_winHeight = 300
         cmds.window('dpCustomAttributesWindow', title=self.langDic[self.langName]['m212_customAttr']+" "+str(DPCUSTOMATTR_VERSION), widthHeight=(customAttributes_winWidth, customAttributes_winHeight), menuBar=False, sizeable=True, minimizeButton=True, maximizeButton=False)
-        cmds.showWindow('dpCustomAttributesWindow')
         # create UI layout and elements:
         customAttributesLayout = cmds.columnLayout('customAttributesLayout', adjustableColumn=True, columnOffset=("both", 10))
         mainLayout = cmds.columnLayout('mainLayout', adjustableColumn=True, columnOffset=("both", 10), parent=customAttributesLayout)
         cmds.text("headerTxt", label=self.langDic[self.langName]['i267_customHeader']+' "'+ATTR_START+'"', align="left", height=30, font='boldLabelFont', parent=mainLayout)
         # selection collection
         self.selectionCollection = cmds.radioCollection('selectionCollection', parent=mainLayout)
-        cmds.radioButton(label=self.langDic[self.langName]['i211_all'].capitalize(), annotation="all", onCommand=self.populateItems)
-        cmds.radioButton(label=self.langDic[self.langName]['i266_selected'], annotation="selected", onCommand=self.populateItems)
-        existing = cmds.radioButton(label=self.langDic[self.langName]['m071_existing'], annotation="existing", onCommand=self.populateItems)
+        all = cmds.radioButton(label=self.langDic[self.langName]['i211_all'].capitalize(), annotation="all", onCommand=self.changeItemSelectionConnection)
+        selected = cmds.radioButton(label=self.langDic[self.langName]['i266_selected'], annotation="selected", onCommand=self.changeItemSelectionConnection)
+#        existing = cmds.radioButton(label=self.langDic[self.langName]['m071_existing'], annotation="existing", onCommand=self.changeItemSelectionConnection)
+        
         # items and attributes layout
-        tablePaneLayout = cmds.paneLayout("tablePaneLayout", configuration="vertical2", separatorThickness=2, parent=mainLayout)
-        leftColumnLayout = cmds.columnLayout('leftColumnLayout', adjustableColumn=True, columnOffset=("both", 2), parent=tablePaneLayout)
-        cmds.separator(height=5, style="none", parent=leftColumnLayout)
-        self.itemFilterTF = cmds.textField("itemFilterTF", text="", changeCommand=self.populateItems, parent=leftColumnLayout)
-        cmds.separator(height=5, style="none", parent=leftColumnLayout)
-        self.itemSL = cmds.textScrollList("itemSL", width=30, allowMultiSelection=True, selectCommand=self.actualizeFooter, parent=leftColumnLayout)
-        # attribute layout
-        self.rightColumnLayout = cmds.columnLayout('rightColumnLayout', adjustableColumn=True, columnOffset=("both", 2), parent=tablePaneLayout)
+        tablePaneLayout = cmds.paneLayout("tablePaneLayout", parent=mainLayout)
+        itemSC = cmds.selectionConnection(activeList=True)
+        self.mainSSE = cmds.spreadSheetEditor(mainListConnection=itemSC, filter=self.itemF, attrRegExp=ATTR_START, parent=tablePaneLayout)
         
         # bottom layout for buttons
         buttonLayout = cmds.rowColumnLayout("buttonLayout", numberOfColumns=4, columnWidth=[(1, 60), (2, 60), (3, 100), (4, 60)], parent=mainLayout)
         cmds.button("addButton", label=self.langDic[self.langName]['i063_skinAddBtn'], backgroundColor=(0.6, 0.6, 0.6), parent=buttonLayout)
         cmds.button("removeButton", label=self.langDic[self.langName]['i064_skinRemBtn'], backgroundColor=(0.4, 0.4, 0.4), parent=buttonLayout)
         cmds.text("", parent=buttonLayout)
-        cmds.button("refreshButton", label=self.langDic[self.langName]['m181_refresh'], backgroundColor=(0.5, 0.5, 0.5), command=self.populateItems, parent=buttonLayout)
-        # set ui
-        cmds.radioCollection(self.selectionCollection, edit=True, select=existing)
-
+        cmds.button("refreshButton", label=self.langDic[self.langName]['m181_refresh'], backgroundColor=(0.5, 0.5, 0.5), parent=buttonLayout)
         
-    def jobChangeSelection(self, *args):
-        """ Create a scriptJob to read the selection changing to update the UI.
-        """
-        cmds.scriptJob(event=('SelectionChanged', self.refreshUI), parent='dpCustomAttributesWindow', replacePrevious=True, killWithScene=True, compressUndo=True, force=True)
-    
-    
-    def populateItems(self, *args):
-        """ Fill the items scroll list element with the choosen option (all, selected or existing).
-        """
-        # get current selection type (all, selected or existing custom attributes)
-        selectionRadioButton = cmds.radioCollection(self.selectionCollection, query=True, select=True)
-        radioButtonAnnotation = cmds.radioButton(selectionRadioButton, query=True, annotation=True)
-        # fill conform
-        if radioButtonAnnotation == "all":
-            self.updateItemsList(cmds.ls(selection=False, type="transform"))
-        elif radioButtonAnnotation == "selected":
-            self.updateItemsList(cmds.ls(selection=True, type="transform"))
-        elif radioButtonAnnotation == "existing":
-            self.updateItemsList(self.getExistingList())
-        self.populateAttrs()
+        # set ui
+        cmds.radioCollection(self.selectionCollection, edit=True, select=selected)
+        cmds.showWindow('dpCustomAttributesWindow')
+
+
 
 
     def getExistingList(self, *args):
@@ -139,7 +145,7 @@ class CustomAttr(object):
         """
         self.itemList = list(set(thisList) - set(IGNORE_LIST))
         self.itemList.sort()
-        cmds.textScrollList(self.itemSL, edit=True, removeAll=True)
+        #cmds.textScrollList(self.itemSL, edit=True, removeAll=True)
         if thisList:
             self.filterList(thisList)
             cmds.textScrollList(self.itemSL, edit=True, append=self.itemList)
@@ -189,6 +195,8 @@ class CustomAttr(object):
         """
         """
         customAttrList = []
+        if not thisList:
+            thisList = cmds.ls(selection=False, type="transform")
         if thisList:
             for item in thisList:
                 currentItemAttrList = cmds.listAttr(item)
