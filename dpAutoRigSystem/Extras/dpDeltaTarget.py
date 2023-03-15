d0861f838f28fdaeca9ae7ac141848c909adb8db
Danilo Pinheiro <nilouco@gmail.com>
2023-03-15 16:51:59 -0300

    N648 Delta target wip cube line
    WIP: how to create a polygon line!
    Co-Authored-By: André Rüegger <36240378+Megabrean@users.noreply.github.com>
    Co-Authored-By: SaulAlm <96533080+SaulAlm@users.noreply.github.com>
    Co-Authored-By: Caio Hidaka <61671684+caiohidaka@users.noreply.github.com>

--- dpAutoRigSystem/Extras/dpDeltaTarget.py
+++ dpAutoRigSystem/Extras/dpDeltaTarget.py
@@ -94,10 +94,176 @@ class DeltaTarget(object):
             mel.eval("warning \""+self.langDic[self.langName]["i062_notFound"]+" "+item+"\";")
         return isGeometry
     
-    
-    def dpRunDeltaExtractor(self, originalData, posedData, fixedData, *args):
+
+    def getMeshShape(self, node, *args):
+        """
+        """
+        print("Carretos in mesh")
+        if cmds.objectType(node) != "mesh":
+            childrenList = cmds.listRelatives(node, children=True, type="mesh")
+            if childrenList:
+                return childrenList[0]
+        elif cmds.objectType(node) == "mesh":
+            return node
+        
+
+    def getFather(self, node, *args):
+        """
+        """
+        fatherList = cmds.listRelatives(node, allParents=True, type="transform")
+        if fatherList:
+            return fatherList[0]
+
+
+    def dpRunDeltaExtractor(self, originalNode, posedNode, fixedNode, *args):
         """ Create the delta target.
         """
-        print("Monza Bonina =", originalData, posedData, fixedData)
-        deltaTgt = cmds.duplicate(originalData, name=originalData+"_Delta_Tgt")
+        # declaring variables
+        deltaTransformDic, posedTransformDic, fixedTransformDic = {}, {}, {}
+        attrList = ['tx', 'ty', 'tz', 'rx', 'ry', 'rz', 'sx', 'sy', 'sz']
+
+
+        print("Monza Bonina =", originalNode, posedNode, fixedNode)
+        deltaNode = cmds.duplicate(originalNode, name=originalNode+"_Delta_Tgt")[0]
+        try:
+            cmds.parent(deltaNode, world=True)
+        except:
+            pass
+        
+        deltaMesh = self.getMeshShape(deltaNode)
+        posedMesh = self.getMeshShape(posedNode)
+        fixedMesh = self.getMeshShape(fixedNode)
+        print("Kombi Bonina MESHES =", deltaMesh, posedMesh, fixedMesh)
+
+        # storing transformation data
+        for attr in attrList:
+            deltaTransformDic[attr] = cmds.getAttr(deltaNode+"."+attr)
+            posedTransformDic[attr] = cmds.getAttr(posedNode+"."+attr)
+            fixedTransformDic[attr] = cmds.getAttr(fixedNode+"."+attr)
+            
+
+
+
+
+
+
+        # WIP
+        # get list of mesh vertice proccess
+        # selecting meshes
+        cmds.select([deltaMesh, posedMesh, fixedMesh])
+        meshList = om.MSelectionList()
+        om.MGlobal_getActiveSelectionList(meshList)
+        
+        # declaring from and to objects, dagPaths and vertice lists
+        deltaObject = om.MObject()
+        deltaDagPath = om.MDagPath()
+        deltaVerticeList = om.MPointArray()
+        
+        posedObject = om.MObject()
+        posedDagPath = om.MDagPath()
+        posedVerticeList = om.MPointArray()
+        
+        fixedObject = om.MObject()
+        fixedDagPath = om.MDagPath()
+        fixedVerticeList = om.MPointArray()
+
+        # getting dagPaths
+        meshList.getDagPath(0, deltaDagPath, deltaObject)
+        meshList.getDagPath(1, posedDagPath, posedObject)
+        meshList.getDagPath(2, fixedDagPath, fixedObject)
+        # getting open maya API mesh
+        deltaMeshFn = om.MFnMesh(deltaDagPath)
+        posedMeshFn = om.MFnMesh(posedDagPath)
+        fixedMeshFn = om.MFnMesh(fixedDagPath)
+
+
+
+        # WIP 2
+
+        # verify the same number of vertices
+        if deltaMeshFn.numVertices() == posedMeshFn.numVertices() and deltaMeshFn.numVertices() == fixedMeshFn.numVertices():
+            print ("YES ai funciona segundo o Andre Megabrean!")
+            
+
+            # getting fromTransform father
+            posedFather = self.getFather(posedNode)
+            fixedFather = self.getFather(fixedNode)
+            if posedFather:
+                cmds.parent(posedNode, world=True)
+            if fixedFather:
+                cmds.parent(fixedNode, world=True)
+
+            
+
+
+
+
+
+            # put fromTransform in the same location then toTransform
+           
+            for attr in attrList:
+                cmds.setAttr(deltaNode+"."+attr, lock=False)
+                cmds.setAttr(posedNode+"."+attr, lock=False)
+                cmds.setAttr(fixedNode+"."+attr, lock=False)
+                if not "s" in attr:
+                    cmds.setAttr(deltaNode+"."+attr, 0)
+                    cmds.setAttr(posedNode+"."+attr, 0)
+                    cmds.setAttr(fixedNode+"."+attr, 0)
+                else:
+                    cmds.setAttr(deltaNode+"."+attr, 1)
+                    cmds.setAttr(posedNode+"."+attr, 1)
+                    cmds.setAttr(fixedNode+"."+attr, 1)
+            cmds.delete(cmds.parentConstraint(deltaNode, posedNode, maintainOffset=False))
+            cmds.delete(cmds.scaleConstraint(deltaNode, posedNode, maintainOffset=False))
+            cmds.delete(cmds.parentConstraint(deltaNode, fixedNode, maintainOffset=False))
+            cmds.delete(cmds.scaleConstraint(deltaNode, fixedNode, maintainOffset=False))
+            
+
+            
+            # getting vertices as points
+            deltaMeshFn.getPoints(deltaVerticeList)
+            posedMeshFn.getPoints(posedVerticeList)
+            fixedMeshFn.getPoints(fixedVerticeList)
+            
+            
+
+
+            # progress window
+            progressAmount = 0
+            cmds.progressWindow(title='Match Mesh Data', progress=progressAmount, status='Tranfering: 0%', isInterruptable=True)
+            cancelled = False
+            
+            # transfer vetex position from FROM mesh to TO mesh selected
+            nbVertice = fixedVerticeList.length()
+            for i in range(0, fixedVerticeList.length()):
+                # update progress window
+                progressAmount += 1
+                # check if the dialog has been cancelled
+                if cmds.progressWindow(query=True, isCancelled=True):
+                    cancelled = True
+                    break
+                cmds.progressWindow(edit=True, maxValue=nbVertice, progress=progressAmount, status=('Transfering: ' + repr(progressAmount) + ' vertex'))
+                
+                # transfer data
+                deltaX = fixedVerticeList[i].x - posedVerticeList[i].x
+                deltaY = fixedVerticeList[i].y - posedVerticeList[i].y
+                deltaZ = fixedVerticeList[i].z - posedVerticeList[i].z
+                cmds.move(deltaX, deltaY, deltaZ, deltaMesh+".vtx["+str(i)+"]", absolute=True)
+            
+            cmds.progressWindow(endProgress=True)
+
+
+
+
+
+            if posedFather:
+                cmds.parent(posedNode, posedFather)
+            if fixedFather:
+                cmds.parent(fixedNode, fixedFather)
+            # restore transformation data
+            for attr in attrList:
+                cmds.setAttr(originalNode+"."+attr, deltaTransformDic[attr])
+                cmds.setAttr(posedNode+"."+attr, posedTransformDic[attr])
+                cmds.setAttr(fixedNode+"."+attr, fixedTransformDic[attr])
+            
 
