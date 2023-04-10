087194bd01d68a4773056703430bdb42f461885a
Caio Hidaka <caio@hidaka.com.br>
2023-04-10 12:37:01 -0300

    N559-remove-namespaces
    Adjusted Verify mode to not include "_ dpAR_ " guides in the list.
    This commit is good for tests.

--- dpAutoRigSystem/Validator/CheckIn/dpNamespaceCleaner.py
+++ dpAutoRigSystem/Validator/CheckIn/dpNamespaceCleaner.py
@@ -2,7 +2,6 @@
 from maya import cmds
 from .. import dpBaseValidatorClass
 from importlib import reload
-from ...import dpAutoRig
 reload(dpBaseValidatorClass)
 
 # global variables to this module:    
@@ -23,6 +22,9 @@ class NamespaceCleaner(dpBaseValidatorClass.ValidatorStartClass):
         dpBaseValidatorClass.ValidatorStartClass.__init__(self, *args, **kwargs)
     
     def removeNamespace(self, *args):
+        """ This function will use recursive method to remove all namespace, 
+            when it's not a guide namespace
+        """
         cmds.namespace(setNamespace=':')
         namespaceList = cmds.namespaceInfo(listOnlyNamespaces=True, recurse=True)
         for name in namespaceList:
@@ -47,34 +49,41 @@ class NamespaceCleaner(dpBaseValidatorClass.ValidatorStartClass):
         self.startValidation()
     
         
-
         # ---
         # --- validator code --- beginning
         if objList:
-            toCheckList = objList
+            namespaceListToClean = objList
         else:
-            namespaceWithGuidesList = []
-            namespaceWithoutGuidesList = []
+            namespaceWithGuidesListMain = []
+            namespaceWithoutGuidesListMain = []
             cmds.namespace(setNamespace=':')
             namespaceListMain = cmds.namespaceInfo(listOnlyNamespaces=True)
             if namespaceListMain:
                 for namespace in namespaceListMain:
                     if namespace != "UI" and namespace != "shared":
+                        # check if there's dpGuides in the list
                         types = cmds.namespaceInfo(namespace, listNamespace=True)
                         for type in types:
+                            # if dpGuides, append to list with Guides, else append to withouGuides
                             if type.find("_dpAR_") != -1:
-                                namespaceWithGuidesList.append(namespace)
+                                namespaceWithGuidesListMain.append(namespace)
                             else:
-                                namespaceWithoutGuidesList.append(namespace)
-                for item in namespaceWithoutGuidesList:
-                    if item in namespaceWithGuidesList:
-                        namespaceWithoutGuidesList.remove(item)
-                namespaceWithGuidesList = list(set(namespaceWithGuidesList))
-                namespaceWithoutGuidesList = list(set(namespaceWithoutGuidesList))
-                namespaceListToClean = namespaceWithGuidesList + namespaceWithoutGuidesList
+                                namespaceWithoutGuidesListMain.append(namespace)
+                namespaceWithoutGuidesList = []
+                namespaceWithGuidesList = []
+                # append to new list in order to remove the namespace guide base
+                for namespace in namespaceWithGuidesListMain:
+                    if "_dpAR_" not in namespace:
+                        namespaceWithGuidesList.append(namespace)
+                # append to a new list if not find the item from with guides in without guides
+                for item in namespaceWithoutGuidesListMain:
+                    if item not in namespaceWithGuidesListMain:
+                        namespaceWithoutGuidesList.append(item)
+                
+                # set both list together, excluding the duplicated names
+                namespaceListToClean = list(set(namespaceWithGuidesList)) + list(set(namespaceWithoutGuidesList))
                 
         if namespaceListToClean:
-            print("namespaceListToClean =========", namespaceListToClean)
             progressAmount = 0
             maxProcess = len(namespaceListMain)
             for namespace in namespaceListToClean:
@@ -89,22 +98,18 @@ class NamespaceCleaner(dpBaseValidatorClass.ValidatorStartClass):
                 else: #fix
                     try:
                         if namespace in namespaceWithGuidesList:
-                            print("namespceWithoutGuides ==", namespace)
+                            # call checkImportedGuides from dpAutoRig, to remove namespace when it's guide.
                             self.dpUIinst.checkImportedGuides(False)
-                            
                         else:
-                            print("namespceWithoutGuides ==", namespace)
+                            # call function inside validator, to remove namespaces when it's not a guide.
                             self.removeNamespace()
                             
-                        #raise Exception("Carreto trombado na pista")
                         self.resultOkList.append(True)
                         self.messageList.append(self.dpUIinst.langDic[self.dpUIinst.langName]['v004_fixed']+": "+namespace)
                     except:
                         self.resultOkList.append(False)
                         self.messageList.append(self.dpUIinst.langDic[self.dpUIinst.langName]['v005_cantFix']+": "+namespace)
-#                else:
-#                    self.foundIssueList.append(False)
-#                    self.resultOkList.append(True)
+
         else:
             self.checkedObjList.append("")
             self.foundIssueList.append(False)
