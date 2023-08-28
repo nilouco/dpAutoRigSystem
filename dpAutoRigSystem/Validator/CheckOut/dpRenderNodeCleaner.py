d186ed84b14c4c0552e5b9573c98f256d755c6f1
Danilo Pinheiro <nilouco@gmail.com>
2023-08-27 21:27:48 -0300

    N739 Render node cleaner removing shaders
    Ready to merge and use.
    Maybe still in concept if we'll delete all unused rendering nodes or just shader materials.

--- dpAutoRigSystem/Validator/CheckOut/dpRenderNodeCleaner.py
+++ dpAutoRigSystem/Validator/CheckOut/dpRenderNodeCleaner.py
@@ -1,7 +1,6 @@
 # importing libraries:
 from maya import cmds
 from maya import mel
-import os
 from .. import dpBaseValidatorClass
 
 # global variables to this module:
@@ -42,41 +41,36 @@ class RenderNodeCleaner(dpBaseValidatorClass.ValidatorStartClass):
         if objList:
             toCheckList = objList
         else:
-            toCheckList = cmds.ls(selection=False, type='skinCluster')
+            toCheckList = cmds.ls(selection=False, materials=True)
         if toCheckList:
-            progressAmount = 0
-            maxProcess = len(toCheckList)
-            for item in toCheckList:
-                if self.verbose:
-                    # Update progress window
-                    progressAmount += 1
-                    cmds.progressWindow(edit=True, maxValue=maxProcess, progress=progressAmount, status=(self.dpUIinst.lang[self.title]+': '+repr(progressAmount)))
+            if len(toCheckList) > 3: #discarting default materials
+                # getting data to analyse
+                defaultMatList = ['lambert1', 'standardSurface1', 'particleCloud1']
+                allMatList = list(set(toCheckList) - set(defaultMatList))
+                usedMatList = list(set(self.getUsedMaterialList()) - set(defaultMatList))
                 # conditional to check here
-                influenceList = cmds.skinCluster(item, query=True, influence=True)
-                weightedInfluenceList = cmds.skinCluster(item, query=True, weightedInfluence=True)
-                if not len(influenceList) == len(weightedInfluenceList):
-                    self.checkedObjList.append(item)
+                if not len(allMatList) == len(usedMatList):
+                    progressAmount = 0
+                    maxProcess = len(allMatList)
+                    if self.verbose:
+                        # Update progress window
+                        progressAmount += 1
+                        cmds.progressWindow(edit=True, maxValue=maxProcess, progress=progressAmount, status=(self.dpUIinst.lang[self.title]+': '+repr(progressAmount)))
+                    issueMatList = sorted(list(set(allMatList) - set(usedMatList)))
+                    self.checkedObjList.append(str(", ".join(issueMatList)))
                     self.foundIssueList.append(True)
                     if self.verifyMode:
                         self.resultOkList.append(False)
                     else: #fix
                         try:
-                            # loading mel script with the needed procedure
-                            mel.eval("source \"cleanUpScene\";")
-                            
-                            #https://discourse.techart.online/t/python-optimizescenesize/15341/5
-                            if "MAYA_TESTING_CLEANUP" not in os.environ:
-                                os.environ["MAYA_TESTING_CLEANUP"] = "enable"
-                                mel.eval("scOpt_performOneCleanup( {\"shaderOption\"} );")
-                                del os.environ["MAYA_TESTING_CLEANUP"]
-                            else:
-                                mel.eval("scOpt_performOneCleanup( {\"shaderOption\"} );")
-                            
+                            fixResult = mel.eval("MLdeleteUnused;")
                             self.resultOkList.append(True)
-                            self.messageList.append(self.dpUIinst.lang['v004_fixed']+": "+item)
+                            self.messageList.append(self.dpUIinst.lang['v004_fixed']+": "+str(fixResult)+" nodes = "+str(len(issueMatList))+" materials")
                         except:
                             self.resultOkList.append(False)
-                            self.messageList.append(self.dpUIinst.lang['v005_cantFix']+": "+item)
+                            self.messageList.append(self.dpUIinst.lang['v005_cantFix']+": materials")
+            else:
+                self.notFoundNodes()
         else:
             self.notFoundNodes()
         # --- validator code --- end
@@ -87,3 +81,18 @@ class RenderNodeCleaner(dpBaseValidatorClass.ValidatorStartClass):
         self.reportLog()
         self.endProgressBar()
         return self.dataLogDic
+
+
+    def getUsedMaterialList(self, *args):
+        """ List all materials used by geometry in the scene.
+            https://discourse.techart.online/t/list-all-materials-used-in-scene/10185
+        """
+        usedMaterialList = []
+        shadingEngineList = cmds.ls(type='shadingEngine')
+        for shadEng in shadingEngineList:
+            # if an shadingEngine has 'sets' members, it is used in the scene
+            if cmds.sets(shadEng, query=True):
+                matList = cmds.listConnections('{}.surfaceShader'.format(shadEng))
+                if matList:
+                    usedMaterialList.extend(matList)
+        return list(set(usedMaterialList))
