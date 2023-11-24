9d22578de206c2098992f6991f697e9fe8aa1ffd
Caio Hidaka <caio@hidaka.com.br>
2023-11-24 12:54:57 -0300

    N547-ball-follow-middle-foot
    Added attribute follow on Ball control to space switch with Middle control.

--- dpAutoRigSystem/Modules/dpFoot.py
+++ dpAutoRigSystem/Modules/dpFoot.py
@@ -10,7 +10,7 @@ TITLE = "m024_foot"
 DESCRIPTION = "m025_footDesc"
 ICON = "/Icons/dp_foot.png"
 
-DP_FOOT_VERSION = 2.0
+DP_FOOT_VERSION = 2.1
 
 
 class Foot(dpBaseClass.StartClass, dpLayoutClass.LayoutClass):
@@ -414,6 +414,16 @@ class Foot(dpBaseClass.StartClass, dpLayoutClass.LayoutClass):
                     cmds.connectAttr(footPlantCnd+".outColorR", footPlantInvMD+".input1X", force=True)
                     cmds.connectAttr(footPlantInvMD+".outputX", self.footCtrlZeroList[1]+".rotateX", force=True)
                 
+                # create follow attribute to footBall control to space switch to middle control space:
+                cmds.addAttr(self.RFECtrl, longName="follow", attributeType ="double", min=0, max=1, defaultValue=0, keyable=True)
+                ballGrp = cmds.group(self.RFECtrl, name=self.RFECtrl+"_Follow_Grp")
+                pacBall = cmds.parentConstraint(self.middleFootCtrl, self.RFDCtrl, ballGrp, mo=True, name=ballGrp+"_PaC")[0]
+                cmds.setAttr(pacBall+".interpType", 0)
+                cmds.connectAttr(self.RFECtrl+".follow", pacBall+"."+self.middleFootCtrl+"W0")
+                revNode = cmds.createNode("reverse", name=self.RFECtrl+"_PaC_Rev")
+                cmds.connectAttr(self.RFECtrl+".follow", revNode+".inputX")
+                cmds.connectAttr(revNode+".outputX", pacBall+"."+self.RFDCtrl+"W1")
+
                 # organizing keyable attributes:
                 self.ctrls.setLockHide([self.middleFootCtrl, self.footCtrl], ['v'], l=False)
                 
