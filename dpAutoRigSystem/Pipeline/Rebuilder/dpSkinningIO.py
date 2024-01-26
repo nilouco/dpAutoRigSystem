c18bb150c625dcebce11c5382e8e1b1d747a489a
Danilo Pinheiro <nilouco@gmail.com>
2024-01-25 21:48:47 -0300

    N790 Rebuilder dpSkinningIO start coding
    Just started the code for the new rebuilder module dpSkinningIO.
    We have some placeholders at the moment.

---
+++ dpAutoRigSystem/Pipeline/Rebuilder/dpSkinningIO.py
@@ -0,0 +1,123 @@
+# importing libraries:
+from maya import cmds
+from .. import dpBaseActionClass
+
+# global variables to this module:
+CLASS_NAME = "SkinningIO"
+TITLE = "r016_skinningIO"
+DESCRIPTION = "r017_skinningIODesc"
+ICON = "/Icons/dp_skinningIO.png"
+
+DP_SKINNINGIO_VERSION = 1.0
+
+
+class SkinningIO(dpBaseActionClass.ActionStartClass):
+    def __init__(self, *args, **kwargs):
+        #Add the needed parameter to the kwargs dict to be able to maintain the parameter order
+        kwargs["CLASS_NAME"] = CLASS_NAME
+        kwargs["TITLE"] = TITLE
+        kwargs["DESCRIPTION"] = DESCRIPTION
+        kwargs["ICON"] = ICON
+        self.version = DP_SKINNINGIO_VERSION
+        dpBaseActionClass.ActionStartClass.__init__(self, *args, **kwargs)
+        self.ioDir = "s_skinningIO"
+        self.startName = "dpSkinning"
+    
+
+    def runAction(self, firstMode=True, objList=None, *args):
+        """ Main method to process this validator instructions.
+            It's in export mode by default.
+            If firstMode parameter is False, it'll run in import mode.
+            Returns dataLog with the validation result as:
+                - checkedObjList = node list of checked items
+                - foundIssueList = True if an issue was found, False if there isn't an issue for the checked node
+                - resultOkList = True if well done, False if we got an error
+                - messageList = reported text
+        """
+        # starting
+        self.firstMode = firstMode
+        self.cleanUpToStart()
+        
+        # ---
+        # --- rebuilder code --- beginning
+        # ensure file has a name to define dpData path
+        if not cmds.file(query=True, sceneName=True):
+            self.notWorkedWellIO(self.dpUIinst.lang['i201_saveScene'])
+        else:
+            # load alembic plugin
+            if self.utils.checkLoadedPlugin("AbcExport") and self.utils.checkLoadedPlugin("AbcImport"):
+                self.ioPath = self.getIOPath(self.ioDir)
+                if self.ioPath:
+                    if self.firstMode: #export
+                        meshList = None
+                        if objList:
+                            meshList = objList
+                        else:
+                            meshList = self.getModelToExportList()
+                        if meshList:
+                            progressAmount = 0
+                            maxProcess = len(meshList)
+                            if self.verbose:
+                                # Update progress window
+                                progressAmount += 1
+                                cmds.progressWindow(edit=True, maxValue=maxProcess, progress=progressAmount, status=(self.dpUIinst.lang[self.title]+': '+repr(progressAmount)))
+                            try:
+                                # export alembic
+                                self.pipeliner.makeDirIfNotExists(self.ioPath)
+                                ioItems = ' -root '.join(meshList)
+                                abcName = self.ioPath+"/"+self.startName+"_"+self.pipeliner.getCurrentFileName()+".abc"
+                                cmds.AbcExport(jobArg="-frameRange 0 0 -uvWrite -writeVisibility -writeUVSets -worldSpace -dataFormat ogawa -root "+ioItems+" -file "+abcName)
+                                self.wellDoneIO(', '.join(meshList))
+                            except:
+                                self.notWorkedWellIO(', '.join(meshList))
+                        else:
+                            self.notWorkedWellIO("Render_Grp")
+                    else: #import
+                        exportedList = self.getExportedList()
+                        if exportedList:
+                            try:
+                                # import alembic
+                                exportedList.sort()
+                                abcToImport = self.ioPath+"/"+exportedList[-1]
+                                #cmds.AbcImport(jobArg="-mode import \""+abcToImport+"\"")
+                                
+                                # clean up geometries
+                                validatorToRunList = ["dpUnlockNormals", "dpSoftenEdges", "dpFreezeTransform", "dpGeometryHistory"]
+                                self.runActionsInSilence(validatorToRunList, self.dpUIinst.checkInInstanceList, False) #fix
+                                self.wellDoneIO(exportedList[-1])
+                            except:
+                                self.notWorkedWellIO(exportedList[-1])
+                        else:
+                            self.notWorkedWellIO(self.dpUIinst.lang['r007_notExportedData'])
+                else:
+                    self.notWorkedWellIO(self.dpUIinst.lang['r010_notFoundPath'])
+            else:
+                self.notWorkedWellIO(self.dpUIinst.lang['e022_notLoadedPlugin']+"AbcExport")
+        # --- rebuilder code --- end
+        # ---
+
+        # finishing
+        self.updateButtonColors()
+        self.reportLog()
+        self.endProgressBar()
+        return self.dataLogDic
+
+
+    def getModelToExportList(self, *args):
+        """ Returns a list of higher father mesh node list or the children nodes in Render_Grp.
+        """
+        meshList = []
+        renderGrp = self.utils.getNodeByMessage("renderGrp")
+        if renderGrp:
+            meshList = cmds.listRelatives(renderGrp, allDescendents=True, fullPath=True, noIntermediate=True, type="mesh") or []
+            if meshList:
+                return cmds.listRelatives(renderGrp, children=True, type="transform")
+        if not meshList:
+            unparentedMeshList = cmds.ls(selection=False, noIntermediate=True, long=True, type="mesh")
+            if unparentedMeshList:
+                for item in unparentedMeshList:
+                    fatherNode = item[:item[1:].find("|")+1]
+                    if fatherNode:
+                        if not fatherNode in meshList:
+                            meshList.append(fatherNode)
+                return meshList
