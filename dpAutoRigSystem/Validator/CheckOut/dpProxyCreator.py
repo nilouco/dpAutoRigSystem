ddd8be4ac7d148fa35265c37def443369ce9bbe7
Caio Hidaka <caio@hidaka.com.br>
2024-01-26 22:45:11 -0300

    N799-add-fullpath-proxy-creator
    Added new flag fullPath=True, and lockNode=False, to avoid geometries with different shape name.
    
    Co-Authored-By: Danilo Pinheiro <nilouco@gmail.com>

--- dpAutoRigSystem/Validator/CheckOut/dpProxyCreator.py
+++ dpAutoRigSystem/Validator/CheckOut/dpProxyCreator.py
@@ -161,8 +161,9 @@ class ProxyCreator(dpBaseValidatorClass.ValidatorStartClass):
                                 nodeFaceList.append(source+".f["+str(n)+"]")
                         # create proxy geometry
                         dup = cmds.duplicate(source, name=source+"_"+jnt+"_Pxy")[0]
-                        for dupItem in cmds.listRelatives(dup, children=True, allDescendents=True):
+                        for dupItem in cmds.listRelatives(dup, children=True, allDescendents=True, fullPath=True):
                             if "Orig" in dupItem:
+                                cmds.lockNode(dupItem, lock=False)
                                 cmds.delete(dupItem)
                         if nodeFaceList:
                             faceDupList = [w.replace(source, dup) for w in nodeFaceList]
