594d1f18455dff0723713c46946fd269635ff231
SaulAlm <salmmotion@gmail.com>
2025-06-26 17:59:51 -0300

    N909 - Start the hideAllJoins script

--- dpAutoRigSystem/dpAutoRig.py
+++ dpAutoRigSystem/dpAutoRig.py
@@ -18,8 +18,8 @@
 ###################################################################
 
 
-DPAR_VERSION_5 = "5.00.17"
-DPAR_UPDATELOG = "N902 - Passthrough attributes new checkout validator."
+DPAR_VERSION_5 = "5.00.19"
+DPAR_UPDATELOG = "N909 - Hide joints check-out."
 
 # to make old dpAR version compatible to receive this update message - it can be deleted in the future 
 DPAR_VERSION_PY3 = "5.00.00 - ATTENTION !!!\n\nThere's a new dpAutoRigSystem released version.\nBut it isn't compatible with this current version 4, sorry.\nYou must download and replace all files manually.\nPlease, delete the folder and copy the new one.\nAlso, recreate your shelf button with the given code in the _shelfButton.txt\nThanks."
