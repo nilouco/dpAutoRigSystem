e28230157901786b8e790bddd996b53e3d833a98
Danilo Pinheiro <nilouco@gmail.com>
2025-07-17 08:49:44 -0300

    N922 - fixed optCtrl instance without node
    Fixed trying connect optionCtrl message in the guideNet if there isn't an optionCtrl node in a new scene while running an old dpAR instance.

--- dpAutoRigSystem/Modules/Base/dpBaseStandard.py
+++ dpAutoRigSystem/Modules/Base/dpBaseStandard.py
@@ -587,7 +587,8 @@ class BaseStandard(object):
         cmds.lockNode(self.guideNet, lock=False)
         cmds.connectAttr(self.guideNet+".message", self.moduleGrp+".net", force=True)
         if self.dpUIinst.optionCtrl:
-            cmds.connectAttr(self.dpUIinst.optionCtrl+".message", self.guideNet+".linkedNode", force=True)
+            if cmds.objExists(self.dpUIinst.optionCtrl):
+                cmds.connectAttr(self.dpUIinst.optionCtrl+".message", self.guideNet+".linkedNode", force=True)
         else:
             cmds.connectAttr(self.moduleGrp+".message", self.guideNet+".linkedNode", force=True)
         self.addNodeToGuideNet([self.moduleGrp, self.radiusCtrl, self.annotation], ["moduleGrp", "radiusCtrl", "annotation"])
