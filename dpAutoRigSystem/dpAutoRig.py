a835fab62ef45460dcdc9bfbbe45f7c812cc683a
caiohidaka <61671684+caiohidaka@users.noreply.github.com>
2026-02-12 16:36:50 -0300

    N910 - Updating version
    Last version with correction in Rebuild Tgts. Still need some tests. Danilo will help with those issues.
    
    Co-Authored-By: Danilo Pinheiro <nilouco@gmail.com>

--- dpAutoRigSystem/dpAutoRig.py
+++ dpAutoRigSystem/dpAutoRig.py
@@ -19,7 +19,7 @@
 
 
 DPAR_VERSION_5 = "5.01.43"
-DPAR_UPDATELOG = "N910 - Facial Connections new features."
+DPAR_UPDATELOG = "N910 - Facial Connections new features.\nN142 - Rebuild blendShapes tool"
 
 # to make old dpAR version compatible to receive this update message - it can be deleted in the future 
 DPAR_VERSION_PY3 = "5.00.00 - ATTENTION !!!\n\nThere's a new dpAutoRigSystem released version.\nBut it isn't compatible with this current version 4, sorry.\nYou must download and replace all files manually.\nPlease, delete the folder and copy the new one.\nAlso, recreate your shelf button with the given code in the _shelfButton.txt\nThanks."
