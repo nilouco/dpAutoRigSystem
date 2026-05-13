9300f55034392de55ff77483e4f261268aee468c
SaulAlmeida <salmmotion@gmail.com>
2026-05-13 12:09:08 -0300

    N990 - add minValue and max value for the facial calibrate panel.
    Add minValue and maxValue for the facial calibrate panel.

--- dpAutoRigSystem/Tools/dpFacialConnection.py
+++ dpAutoRigSystem/Tools/dpFacialConnection.py
@@ -745,7 +745,7 @@ class FacialConnection(object):
             offsetCtrl = calibAttr[:calibAttr.rfind("_")]
             offsetCtrl = offsetCtrl[:offsetCtrl.rfind("_")]+"_Ctrl"
             calibAttrValue = cmds.getAttr(ctrl+"."+calibAttr)
-            cmds.floatSliderButtonGrp(calibAttr+"_fsbg", label=calibAttr, field=True, buttonLabel=self.dpUIinst.lang['m004_select'], value=calibAttrValue, dragCommand=partial(cmds.setAttr, ctrl+"."+calibAttr), changeCommand=partial(cmds.setAttr, ctrl+"."+calibAttr), buttonCommand=partial(cmds.select, offsetCtrl), parent='attrsLayout')
+            cmds.floatSliderButtonGrp(calibAttr+"_fsbg", label=calibAttr, field=True, buttonLabel=self.dpUIinst.lang['m004_select'], value=calibAttrValue, minValue=-100.0, maxValue=100.0, dragCommand=partial(cmds.setAttr, ctrl+"."+calibAttr), changeCommand=partial(cmds.setAttr, ctrl+"."+calibAttr), buttonCommand=partial(cmds.select, offsetCtrl), parent='attrsLayout')
 
 
     def getFacialCalibrationAttrList(self, ctrl, attr, *args):
