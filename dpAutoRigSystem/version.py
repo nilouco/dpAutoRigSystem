f7e402ae9e838b4dc7f0dbd0adca9ecfbd37ac6e
Danilo Pinheiro <nilouco@gmail.com>
2026-09-24 20:32:10 -0300

    Update version.py
    Resolved conflits.

--- dpAutoRigSystem/version.py
+++ dpAutoRigSystem/version.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python3
 """Package declaring dpAutoRigSystem version: Major.Minor.Patch"""
 
-<<<<<<< Updated upstream
-__version__: str = '5.02.16'
-=======
 __version__: str = '5.02.17'
->>>>>>> Stashed changes
 _update_log: str = 'Refactory.'
